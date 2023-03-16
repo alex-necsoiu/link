46bc052208caab24e7e31c525deee508c02f575f
Ioan Alexandru Necsoiu <alex@MacBook-Pro-de-Ioan-3.local>
2023-03-16 16:28:42 +0100

    fix: Fix path error

--- pkg/core/main.go
+++ pkg/core/main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+
+	str "github.com/alex-necsoiu/link/datastructures"
 )
 
 func main() {
 
 	fmt.Println("I tu padre que tal?")
 	// firststack := d.Stack
-	// // Stack usage example
-	// stack := &Stack{}
-	// stack.Push(1)
-	// stack.Push(2)
-	// stack.Push(3)
-	// fmt.Println(stack.Pop()) // Prints 3
-	// fmt.Println(stack.Pop()) // Prints 2
+	// Stack usage example
+	stack := &str.Stack{}
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	fmt.Println(stack.Pop()) // Prints 3
+	fmt.Println(stack.Pop()) // Prints 2
 
-	// // Queue usage example
-	// queue := &Queue{}
-	// queue.Enqueue(1)
-	// queue.Enqueue(2)
-	// queue.Enqueue(3)
-	// fmt.Println(queue.Dequeue()) // Prints 1
-	// fmt.Println(queue.Dequeue()) // Prints 2
+	// Queue usage example
+	queue := &str.Queue{}
+	queue.Enqueue(1)
+	queue.Enqueue(2)
+	queue.Enqueue(3)
+	fmt.Println(queue.Dequeue()) // Prints 1
+	fmt.Println(queue.Dequeue()) // Prints 2
 }
